2cd60165a0f7f537f38549948dcc987c04f17e1e
renanbatko <renanbatkousp@gmail.com>
2015-11-02 16:06:01 -0200

    second commit

--- proj_ing.py
+++ proj_ing.py
@@ -1,4 +1,5 @@
 #/usr/bin/env python
+# -*- coding: utf-8 -*-
 import urllib
 import time
 import re
@@ -11,10 +12,16 @@ def num_pages(page):
             c+= 1
             if (c == 8):
                 print line
+                temp = line.split("page-", 1)[1]
+                temp = temp.split('"', 1)[0]
+                temp = int(temp)
+                return temp
 
 url_base = raw_input("URL do topico: ")
 titulo = raw_input("Titulo do topico: ")
 
 page = urllib.urlopen(url_base)
 num = num_pages(page)
+
+
 page.close()
